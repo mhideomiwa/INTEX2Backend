f45ba1dddef25e65a263c464c451844a0090a3f5
davidwinnbigvideo <st2school@gmail.com>
2024-04-09 15:00:06 -0600

    CRUD is technically there now

--- Data/IBackendRepository.cs
+++ Data/IBackendRepository.cs
@@ -8,5 +8,21 @@ namespace Intex2Backend.Data
         IEnumerable<LineItem> LineItems { get; set; }
         IEnumerable<Order> Orders { get; set; }
         IEnumerable<Product> Products { get; set; }
+
+        void UpdateProduct(Product product) { }
+        void DeleteProduct(Product product) { }
+        void AddProduct(Product product) { }
+
+        void UpdateOrder(Order order) { }
+        void DeleteOrder(Order order) { }
+        void AddOrder(Order order) { }
+
+        void UpdateLineItem(LineItem lineItem) { }
+        void DeleteLineItem(LineItem lineItem) { }
+        void AddLineItem(LineItem lineItem) { }
+
+        void UpdateCustomer(Customer customer) { }
+        void DeleteCustomer(Customer customer) { }
+        void AddCustomer(Customer customer) { }
     }
 }
